11d6871bad5cdadd23ade773d766001d9137b16a
Pawan Kumar <pawankumar49871@gmail.com>
2023-10-25 12:08:46 +0530

    ADD: search functionality added

--- lib/src/utils/global.dart
+++ lib/src/utils/global.dart
@@ -1,3 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 late SharedPreferences sharedPreferences;
+
+String get appName => "Sangrah";
+String get logoPath => "assets/images/icon.png";
+
+showSnackBar(var message, Color color, Color? textColor) {
+  return Get.rawSnackbar(
+      title: appName,
+      messageText: Text(
+        message.toString(),
+      ),
+      // message: message,
+      backgroundColor: color,
+      icon: Container(
+        margin: const EdgeInsets.only(left: 4, right: 4),
+        child: Image.asset(
+          logoPath,
+          fit: BoxFit.cover,
+          // height: 60,
+          width: 150,
+        ),
+      ));
+}
